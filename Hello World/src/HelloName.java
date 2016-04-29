7c2d836becaa24bac9e4421bc21c873c57d3892e
thomas.keeley <thomas.keeley@MU-C1-18.mullen.pvt.k12.co.us>
2016-04-29 08:34:14 -0600

    Initial Commit

---
+++ Hello World/src/HelloName.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HelloName {
+
+	public static void main(String[] args)
+	{
+		Scanner userlnput = new Scanner(System.in);
+		System.out.println("What is your name?");
+		String name = userlnput.nextLine();
+		System.out.println("Hello." + name + "!");
+		String favoriteColor = userlnput.nextLine();
+		System.out.println("wow," + favoriteColor + "is my favorite color too!");
+		String favoriteTeam =
+		System.out.println
+	}
+
+}
---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld 
+	{
+	public static void main(String[] args) 
+		{
+		System.out.println("Hello World!")
+		System.out.println("My name is Thomas")
+		System.out.println("My sport is Basketball");
+	}
+
